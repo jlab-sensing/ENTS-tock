2c1e63fb038aa50b4130348b4320824ad72fee73
John Madden <jmadden173@pm.me>
2026-06-18 08:56:24 -0700

    Added lingering files

---
+++ embedded/stm32/examples/storage/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include <storage/fram.h>
+
+int main(void) {
+  printf("Hello World\n");
+
+  uint8_t write_data[] = {0xDE, 0xAD, 0xBE, 0xEF, 0x42};
+  fram_addr addr        = 0x100;
+
+  fram_status status = fram_write(addr, write_data, sizeof(write_data));
+  printf("fram_write status: %d", status);
+
+  uint8_t read_data[5] = {0};
+  status = fram_read(addr, sizeof(read_data), read_data);
+  printf("fram_read status: %d", status);
+  
+  for (int i = 0; i < 5; i++) {
+    printf("%x ", write_data[i]);
+  }
+  printf("\n");
+
+  for (int i = 0; i < 5; i++) {
+    printf("%x ", read_data[i]);
+  }
+  printf("\n");
+
+  return 0;
+}
