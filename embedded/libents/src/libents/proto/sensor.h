3e22daa104ff69163c7ac750a6ba42faf3e81fb3
John Madden <jmadden173@pm.me>
2026-06-30 16:24:42 -0700

    Added print statement for sensor measurements
    Prepush

--- embedded/libents/src/libents/proto/sensor.h
+++ embedded/libents/src/libents/proto/sensor.h
@@ -221,6 +221,10 @@ SensorStatus DecodeRepeatedSensorReponses(const uint8_t* data, const size_t len,
  */
 SensorStatus CheckSensorResponse(const SensorResponse* resp);
 
+void PrintSensorMeasurement(const SensorMeasurement* m);
+
+void PrintRepeatedSensorMeasurements(const RepeatedSensorMeasurements* msg);
+
 /**
  * @}
  */
