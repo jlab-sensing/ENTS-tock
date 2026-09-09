09f294732440ad1d46eb8a6cefd04b91edfb022a
John Madden <jmadden173@pm.me>
2026-09-09 14:57:55 -0700

    Add support for smaller sized prod kernel

--- embedded/kernel.sh
+++ embedded/kernel.sh
@@ -6,12 +6,27 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Installing kernel"
 pushd $SCRIPT_DIR/extras/tock/boards/lora_e5_mini > /dev/null
+
+# reset chip layout
+git restore chip_layout.ld
+
+
 if [ "${DEV-}" == "true" ]; then
     echo "Development build"
-    echo "NOT IMPLEMENTED"
+
+    make erase-all
+    make clean    
+    make install dev=1
 else
-    echo "Production build"
+    echo "Production build (no console)"
+
+    # Apply chip layout patch for smaller sized kernel
+    git apply prod-layout.patch
+
+    make erase-all
+    make clean    
     make install
 fi
+
 popd > /dev/null
 echo ""
